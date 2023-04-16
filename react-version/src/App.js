08fe2bdecacaa816e81910de70974bbded74c53e
Jeremy <010JGL@GMAIL.COM>
2023-04-15 21:05:50 -0400

    tweeter to react

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -2,20 +2,21 @@ import Navigation from './components/Navigation'
 import Profile from './components/Profile'
 import TweetList from './components/TweetList'
 import TweetForm from './components/TweetForm'
+import Tweet from './components/Tweet'
 import './App.css';
 
 function App() {
   return (
-    <body>
+    <>
       <nav><Navigation /> </nav>
       <aside><Profile /> </aside>
       <main className="container">
         <section className="newTweet"><TweetForm /></section>
-        <section className="tweets"><TweetList /></section>
+        <section className="tweets"><TweetList /><Tweet /><Tweet /></section>
       </main>
 
       {/* <div className="App"> */}
-    </body>
+    </>
     
 
   );
--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,15 +1,20 @@
 import React from 'react'; //optional
-
-function TweetForm(){
+$( document ).ready(function() {
+function TweetForm() {
+  const text = "What are you humming about?"
+  
+  $("#newtweet__form").submit(function(handler) {
+    handler.preventDefault();
+  });
   return (
-    <section class="newtweet">
-    <form method="post" action="/tweets" class="newtweet__form">
-      <textarea class="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
-      <input type="submit" value="Tweet" class="form__input" />
-      <span class="form__counter">140</span>
-    </form>
-  </section>
+    <section className="newtweet">
+      <form id="newtweet__form" method="post" action="/tweets" className="newtweet__form">
+        <textarea className="form__textarea" name="text" placeholder={text}></textarea>
+        <input type="submit" value="Tweet" className="form__input" />        
+        <span className="form__counter">140</span>
+      </form>
+    </section>
   );
 }
-
+})
 export default TweetForm;
